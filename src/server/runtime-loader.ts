2311064afd40c3cf4217fba28099880cf2e7f6e5
Justin <shpaw415@gmail.com>
2025-10-23 00:56:10 -0400

    add runtime plugin to fix react in production

--- src/server/runtime-loader.ts
+++ src/server/runtime-loader.ts
@@ -1,12 +1,39 @@
 import { pluginLoader } from "../plugins/plugin-loader";
+import { plugin, type BunPlugin } from "bun";
 
 /**
  * Load runtime plugins using Bun's plugin system.
  */
 export function load() {
-    for (const { name, pluginParent } of pluginLoader.getPluginByName("runtimePlugins")) {
-        pluginParent.forEach((plugin) => {
-            Bun.plugin(plugin);
-        });
-    }
+  for (const { name, pluginParent } of pluginLoader.getPluginByName(
+    "runtimePlugins"
+  )) {
+    pluginParent.forEach((plugin) => {
+      Bun.plugin(plugin);
+    });
+  }
 }
+
+const reactFix: BunPlugin = {
+  name: "React-import-tmp-fix",
+  setup(runtime) {
+    runtime.onLoad(
+      {
+        filter: /\.tsx$/,
+      },
+      async (props) => {
+        const file = await Bun.file(props.path).text();
+
+        return {
+          contents:
+            `
+            import { jsxDEV as jsxDEV_7x81h0kn } from "react/jsx-dev-runtime";
+          ` +
+            file +
+            "\n global.jsxDEV_7x81h0kn = jsxDEV_7x81h0kn;",
+        };
+      }
+    );
+  },
+};
+if (process.env.NODE_ENV == "production") plugin(reactFix);
