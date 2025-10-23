cb42397fa1a2bb6a6f41b22ab7afc7fb62dac6a5
Justin <shpaw415@gmail.com>
2025-10-23 01:03:32 -0400

    add script to packagejson in init project

--- bin/init/index.ts
+++ bin/init/index.ts
@@ -17,7 +17,11 @@ export const PATH_TO_DEFAULT_CONFIG_FILE = join(
 ) as `<cwd>/config.default.ts`;
 
 async function init() {
-  await Promise.all([copyConfigFileToProject(), copyBunfigToProject()]);
+  await Promise.all([
+    copyConfigFileToProject(),
+    copyBunfigToProject(),
+    addScriptsToPackageJson(),
+  ]);
   copyDotFrameMasterDirToProject();
   console.log("frame-master has been initialized in your project.");
 }
@@ -41,6 +45,20 @@ function copyBunfigToProject() {
   );
   const targetPath = join(process.cwd(), "bunfig.toml");
   return Bun.file(targetPath).write(bunfigFile);
+}
+
+async function addScriptsToPackageJson() {
+  const packageJsonPath = join(process.cwd(), "package.json");
+  const packageJsonFile = Bun.file(packageJsonPath);
+  const packageJsonText = await packageJsonFile.text();
+  const packageJson = JSON.parse(packageJsonText);
+
+  packageJson.scripts ??= {};
+  packageJson.scripts["dev"] =
+    "NODE_ENV=development bun --hot frame-master dev";
+  packageJson.scripts["start"] = "NODE_ENV=production bun frame-master start";
+
+  return packageJsonFile.write(JSON.stringify(packageJson, null, 2));
 }
 
 export default init;
--- src/server/runtime-loader.ts
+++ src/server/runtime-loader.ts
@@ -23,14 +23,13 @@ const reactFix: BunPlugin = {
       },
       async (props) => {
         const file = await Bun.file(props.path).text();
-
         return {
-          contents:
-            `
-            import { jsxDEV as jsxDEV_7x81h0kn } from "react/jsx-dev-runtime";
-          ` +
-            file +
-            "\n global.jsxDEV_7x81h0kn = jsxDEV_7x81h0kn;",
+          contents: [
+            `import { jsxDEV as jsxDEV_7x81h0kn } from "react/jsx-dev-runtime";`,
+            file,
+            `global.jsxDEV_7x81h0kn = jsxDEV_7x81h0kn;`,
+            `global.jsxs_eh6c78nj = jsxDEV_7x81h0kn;`,
+          ].join("\n"),
         };
       }
     );
