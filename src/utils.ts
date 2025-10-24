33a0f5b4432178323ed7fa2e71e78bc467a0a59e
Justin <shpaw415@gmail.com>
2025-10-24 15:23:46 -0400

    add utility onRoute

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,44 @@
 import { directiveToolSingleton } from "./plugins";
+import type { masterRequest } from "./server/request-manager";
 
 export { directiveToolSingleton as directiveManager };
+
+type RouteCallback = (master: masterRequest) => void | Promise<void>;
+type MethodKeys =
+  | "POST"
+  | "GET"
+  | "DELETE"
+  | "PUT"
+  | "PATCH"
+  | "OPTIONS"
+  | "HEAD";
+
+/**
+ * to use on router plugin
+ * @example
+ * router: {
+ *   request: (master) =>
+ *      onRoute(master, {
+ *          "/api/some_route": {
+ *              POST: (master) => { ... },
+ *              GET: (master) => { ... }
+ *         },
+ *         "/api/another_route": (master) => { ... }
+ *    })
+ * }
+ * */
+export function onRoute(
+  master: masterRequest,
+  routes: Record<
+    string,
+    RouteCallback | Partial<Record<MethodKeys, RouteCallback>>
+  >
+) {
+  const currentRoute = routes[master.URL.pathname];
+
+  if (typeof currentRoute == "function") {
+    return currentRoute(master);
+  } else if (currentRoute) {
+    return currentRoute[master.request.method as MethodKeys]?.(master);
+  }
+}
