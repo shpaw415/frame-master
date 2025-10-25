b381820190bf4d2070239918f8a1dac6c9fa10cc
Justin <shpaw415@gmail.com>
2025-10-25 14:12:39 -0400

    add utils

--- src/utils.ts
+++ src/utils.ts
@@ -42,3 +42,24 @@ export function onRoute(
     return currentRoute[master.request.method as MethodKeys]?.(master);
   }
 }
+
+export function join(...parts: string[]) {
+  return parts
+    .map((part, index) => {
+      // Normalize backslashes to forward slashes
+      part = part.replace(/\\/g, "/");
+
+      if (index === 0) {
+        // First part: remove trailing slashes only
+        return part.replace(/\/+$/g, "");
+      } else if (index === parts.length - 1) {
+        // Last part: remove leading slashes only (preserve filenames like index.js)
+        return part.replace(/^\/+/g, "");
+      } else {
+        // Middle parts: remove both leading and trailing slashes
+        return part.replace(/^\/+|\/+$/g, "");
+      }
+    })
+    .filter((part) => part.length > 0)
+    .join("/");
+}
