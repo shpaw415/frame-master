4e8113cdb1d0822169a34382b28c24232e8d60e9
m2tech-bot-01 <m2tech-bot-01@users.noreply.github.com>
2026-08-16 00:23:29 +0000

    test(debug): add preview pipeline fixture

--- packages/frame-master/test_project/frame-master.config.ts
+++ packages/frame-master/test_project/frame-master.config.ts
@@ -146,4 +146,41 @@ export default {
 			},
 		},
 	],
+	debugUIOptions: {
+		pipelines: [
+			{
+				id: "preview",
+				label: "Preview pipeline",
+				plugins: [
+					{
+						name: "debug-ui-demo-preview",
+						version: "1.0.0",
+						build: {
+							buildConfig: {
+								entrypoints: ["./src/admin.ts"],
+								outdir: ".frame-master/debug-pipeline-preview",
+								format: "esm",
+								plugins: [
+									{
+										name: "preview-pipeline-marker",
+										setup(build) {
+											build.onLoad({ filter: /\.ts$/ }, async (args) => {
+												const source =
+													(args.__chainedContents as string) ??
+													(await Bun.file(args.path).text());
+												return {
+													contents: `const __debugPipeline = "preview";\n${source}\nconsole.log(__debugPipeline);`,
+													loader: "ts",
+												};
+											});
+										},
+									},
+								],
+							},
+						},
+					},
+				],
+			},
+		],
+	},
 } satisfies FrameMasterConfig;
